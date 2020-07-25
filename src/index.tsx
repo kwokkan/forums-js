5e63303f437204030696bf0e1d6f7ad08905444d
kwokkan <kwokkan@users.noreply.github.com>
2020-07-25 23:11:02 +0100

    Use next for framework.

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import { App } from "./layouts/App";
-
-render(
-    <App title="Forums JS"></App>,
-    document.getElementById("app")
-);
---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { App } from "../layouts/App";
+
+import "../styles/styles.scss";
+import "../styles/vendor.scss";
+
+export default () => <App title="Forums JS"></App>;
