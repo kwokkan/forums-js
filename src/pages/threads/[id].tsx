3740481ada0635e66d2d95abdcbbda0c9ecd2c71
kwokkan <kwokkan@users.noreply.github.com>
2020-08-21 21:54:41 +0100

    #15: UI for adding new comment when signed in.

--- src/pages/threads/[id].tsx
+++ src/pages/threads/[id].tsx
@@ -1,25 +1,30 @@
+import { getSession } from "next-auth/client";
 import { Fragment } from "react";
-import { IProps, Thread } from "../../layouts/Thread";
+import { IProps } from "../../layouts/Thread";
 import { Title } from "../../layouts/Title";
+import { ThreadManager } from "../../managers/ThreadManager";
 import { getThreadById } from "../../services/threadService";
 import { GetTypedServerSideProps } from "../../types/pageTypes";
 import { parseIntParam } from "../../utils/paramUtil";
 
 export const getServerSideProps: GetTypedServerSideProps<{ id: string }> = async (context) => {
+    const session = await getSession(context);
+
     const id = parseIntParam(context.params?.id);
 
     const thread = await getThreadById(id);
 
     return {
         props: {
-            thread: thread
+            thread: thread,
+            user: session?.forumsUser || null
         }
     };
-}
+};
 
-const Page = ({ thread }: IProps) =>
+const Page = ({ thread, user }: IProps) =>
     <Fragment>
         <Title title={thread.name} />
-        <Thread thread={thread} />
+        <ThreadManager thread={thread} user={user} />
     </Fragment>;
 export default Page;
