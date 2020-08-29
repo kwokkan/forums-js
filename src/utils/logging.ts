057e2f52d9b9d5bf79d9610c5ced729b193bf9b7
kwokkan <kwokkan@users.noreply.github.com>
2020-08-29 20:03:59 +0100

    #36: Add log levels.

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,7 +1,19 @@
-const isDev = process.env.NODE_ENV === "development";
+import { parseIntParam } from "./paramUtil";
+
+const logLevel = parseIntParam(process.env.LOG_LEVEL) ?? 0;
+
+enum LogLevel {
+    Trace = 0,
+    Debug = 1,
+    Information = 2,
+    Warning = 3,
+    Error = 4,
+    Critical = 5,
+    None = 6,
+}
 
 export function logDebug(...args: any[]) {
-    if (isDev) {
+    if (logLevel >= LogLevel.Debug) {
         console.debug(...args);
     }
 }
