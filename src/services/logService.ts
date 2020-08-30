3f259ac71211641d0019a83bfe2682a19f236919
kwokkan <kwokkan@users.noreply.github.com>
2020-08-30 17:26:30 +0100

    #26: Use db logging for Next Auth.

--- src/services/logService.ts
+++ src/services/logService.ts
@@ -1,6 +1,6 @@
 import * as repo from "./../repositories";
 
 export async function addLog(...args: any[]): Promise<void> {
-    const message = JSON.stringify({ ...args });
+    const message = JSON.stringify([...args]);
     await repo.addLog(message);
 }
