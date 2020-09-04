a43496cbd23db49c5ac9594d048b9d1e806320da
kwokkan <kwokkan@users.noreply.github.com>
2020-09-04 22:20:55 +0100

    Bump up log test coverage.

--- src/utils/logging.test.ts
+++ src/utils/logging.test.ts
@@ -1,9 +1,26 @@
-import { logDebug, logError } from "./logging";
 
-test("logDebug", () => {
+beforeEach(() => {
+    jest.resetModules();
+    process.env.LOG_LEVEL = "0";
+});
+
+test("logDebug", async () => {
+    const { logDebug } = await import("./logging");
+
     logDebug("log", "debug", 1);
 });
 
-test("logError", () => {
+test("logError", async () => {
+    const { logError } = await import("./logging");
+
     logError("log", "error", 4);
+});
+
+test("logError with throw", async () => {
+    process.env.LOG_THROW_ON_ERROR = "true";
+    const { logError } = await import("./logging");
+
+    const action = () => logError("my exception");
+
+    expect(action).toThrowError("my exception");
 });
